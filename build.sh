c74f1b0f5c6447b5635edf1366ccf89dff6b2b9a
Sumit Goel <sumitgoelpw@users.noreply.github.com>
2018-03-25 02:39:02 -0700

    version bump

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION='1.0.4'
+VERSION='1.0.6'
 
 declare -a LIST=$(curl -s -H "Content-Type: application/json" https://registry.hub.docker.com/v1/repositories/goel/jekyll/tags | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')
 
@@ -12,6 +12,5 @@ for ITEM in $LIST; do
 done
 
 echo "$DOCKER_PASSWORD" | docker login -u goel --password-stdin
-docker build --compress --rm -t goel/jekyll:$VERSION -f Dockerfile .
-docker run --rm goel/jekyll 'pwd && ls -la'
-docker push goel/jekyll:$VERSION
+docker build --pull --compress --rm -t goel/jekyll:$VERSION -t goel/jekyll:latest -f Dockerfile .
+docker push goel/jekyll
