d44018ab8f99beb339e01d8ff544f62092eb5ded
Sumit Goel <sumitgoelpw@users.noreply.github.com>
2018-03-25 03:17:01 -0700

    add images testing

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION='1.0.6'
+VERSION='1.0.7'
 
 declare -a LIST=$(curl -s -H "Content-Type: application/json" https://registry.hub.docker.com/v1/repositories/goel/jekyll/tags | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')
 
@@ -12,5 +12,23 @@ for ITEM in $LIST; do
 done
 
 echo "$DOCKER_PASSWORD" | docker login -u goel --password-stdin
-docker build --pull --compress --rm -t goel/jekyll:$VERSION -t goel/jekyll:latest -f Dockerfile .
+
+# Build image
+docker build --pull \
+             --compress \
+             --rm \
+             -t goel/jekyll:$VERSION \
+             -t goel/jekyll:latest \
+             --build-arg VERSION=$VERSION \
+             -f Dockerfile .
+
+# Test image
+TEST=$(docker run --rm goel/jekyll:latest bash -c 'cat /tmp/VERSION')
+
+if [ "$VERSION" != "$TEST" ]; then
+  echo 'Version mismatch. Test failed.'
+  exit 1
+fi
+
+# Push image to public docker hub
 docker push goel/jekyll
