ac1870bd0a9a1867ca09e142f63a7721f4e4bbb5
Sumit Goel <sumitgoelpw@users.noreply.github.com>
2018-03-25 03:46:40 -0700

    bump version 1.0.8

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION='1.0.7'
+VERSION='1.0.8'
 
 declare -a LIST=$(curl -s -H "Content-Type: application/json" https://registry.hub.docker.com/v1/repositories/goel/jekyll/tags | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')
 
